30e014cde9676197408d288598cc383dee788d6b
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-02-05 10:38:40 -0500

    Testing

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,15 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: {id: bird.id, name: bird.name, species: bird.species }
+    # render json: birds, only: [:id, :name, :species]
+    # render json: birds, exept: [:created_at, :updated_at]
+    else 
+      render json: {message: 'Bird not found'}
+  end
+end 
 end
